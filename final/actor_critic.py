0124524fb1735bba48dce67809495104e29cf4c5
Klemen Voncina <klemenvon@gmail.com>
2019-02-15 13:26:29 +0100

    Removed gym as a dependency as its not used. Included dep list in README.

--- final/actor_critic.py
+++ final/actor_critic.py
@@ -2,7 +2,6 @@ import os
 os.environ["CUDA_VISIBLE_DEVICES"] = ""
 
 import threading
-import gym
 import multiprocessing
 import numpy as np
 from queue import Queue
@@ -185,32 +184,6 @@ class MasterAgent():
                              '{} A3C.png'.format("traffic")))
     plt.show()
 
-  def play(self):
-    env = gym.make(self.game_name).unwrapped
-    state = env.reset()
-    model = self.global_model
-    model_path = os.path.join(self.save_dir, 'model_{}.h5'.format("traffic"))
-    print('Loading model from: {}'.format(model_path))
-    model.load_weights(model_path)
-    done = False
-    step_counter = 0
-    reward_sum = 0
-
-    try:
-      while not done:
-        env.render(mode='rgb_array')
-        policy, value = model(tf.convert_to_tensor(state[None, :], dtype=tf.float32))
-        policy = tf.nn.softmax(policy)
-        action = np.argmax(policy)
-        state, reward, done, _ = env.step(action)
-        reward_sum += reward
-        print("{}. Reward: {}, action: {}".format(step_counter, reward_sum, action))
-        step_counter += 1
-    except KeyboardInterrupt:
-      print("Received Keyboard Interrupt. Shutting down.")
-    finally:
-      env.close()
-
 
 class Memory:
   def __init__(self):
@@ -385,4 +358,5 @@ if __name__ == '__main__':
   if args.train:
     master.train()
   else:
-    master.play()
+    print("Please use the --train option.")
+    sys.exit()
