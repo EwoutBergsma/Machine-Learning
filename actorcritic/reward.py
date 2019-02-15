3eb6fa7372e5dd2b3efea72174bc382631da595d
Klemen Voncina <klemenvon@gmail.com>
2019-02-15 13:29:54 +0100

    Removed all unessential files

--- actorcritic/reward.py
+++
@@ -1,6 +0,0 @@
-class Reward:
-	def __init(self):
-		self.reward = 0
-
-	def get_reward(self):
-		return self.reward
--- actorcritic_new/state.py
+++
@@ -1,6 +0,0 @@
-class State:
-	def __init__(self, intersection):
-		self.state_list = []
-
-	def get_state(self):
-		return self.state_list
